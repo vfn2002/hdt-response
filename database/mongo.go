953c475e7b2dda2df876e19258929165d11c7655
Victor Nielsen <vfn2002@gmail.com>
2020-01-29 20:38:50 +0100

    error handling for mongo.go

--- database/mongo.go
+++ database/mongo.go
@@ -1,21 +1,28 @@
 package database
 
 import (
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // Connect returns mongoDB client for queries
 func Connect(uri string) *mongo.Client {
 	options := options.Client().ApplyURI(uri)
-	//TODO: Add error handling
-	client, _ := mongo.NewClient(options)
+	client, err := mongo.NewClient(options)
+
+	if err != nil {
+		panic(err)
+	}
 
 	client.Connect(context.Background())
-	//TODO: Add error handling "err :="
-	client.Ping(context.Background(), readpref.Primary())
+
+	err = client.Ping(context.Background(), readpref.Primary())
+
+	if err != nil {
+		panic(err)
+	}
 
 	return client
 }
